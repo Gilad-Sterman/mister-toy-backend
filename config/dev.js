0d985265cfde8e6fa23e6a60696b886da54dee7e
Gilad Sterman <giladsterman1999@gmail.com>
2023-10-01 20:02:56 +0300

    added review support

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,4 @@
 export default {
-    // dbURL: 'mongodb://127.0.0.1:27017',
-    dbURL: 'mongodb+srv://gilad1999:Sterman1999@cluster0.z8fzxeo.mongodb.net/?retryWrites=true&w=majority',
+    dbURL: 'mongodb://127.0.0.1:27017',
     dbName: 'tester_db',
 }
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-    dbURL: 'mongodb+srv://gilad1999:Sterman1999@cluster0.z8fzxeo.mongodb.net/?retryWrites=true&w=majority',
+    dbURL: `mongodb+srv://gilad1999:${process.env.MONGOSEC}@cluster0.z8fzxeo.mongodb.net/?retryWrites=true&w=majority`,
     dbName: 'tester_db',
 }
